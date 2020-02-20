98cba73839367354b88cfb25b24080cf6d4b399f
Farley Lai <farleylai@nec-labs.com>
2020-02-19 22:15:11 -0500

    Add ml.sys.x_available() API

--- ml/sys/functions.py
+++ ml/sys/functions.py
@@ -4,3 +4,9 @@ def add_path(p):
     p = str(p)
     if p not in sys.path:
         sys.path.insert(0, p)
+
+def x_available():
+    from subprocess import Popen, PIPE
+    p = Popen(["xset", "-q"], stdout=PIPE, stderr=PIPE)
+    p.communicate()
+    return p.returncode == 0
