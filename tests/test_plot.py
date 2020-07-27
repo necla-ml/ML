1a9201b1398abf5234f4c620bdf28a6c184aad32
Farley Lai <farleylai@nec-labs.com>
2020-07-27 04:27:59 -0400

    Add test case for plot module

---
+++ tests/test_plot.py
@@ -0,0 +1,18 @@
+from ml import plot, logging
+
+def test_confusion_matrix():
+    import torch
+    from sklearn.metrics import confusion_matrix
+    truth = torch.arange(10)
+    perm = torch.randperm(len(truth))
+    preds = truth[perm]
+    cm = confusion_matrix(truth, preds)
+    fig = plot.confusion_matrix(cm)
+    img = plot.numpy(fig)
+    size = fig.get_size_inches() * fig.dpi
+    assert img.shape == (*size.astype('int'), 4)
+    if False:
+        from ml import cv
+        img = cv.cvtColor(img, cv.COLOR_RGBA2BGR)
+        cv.save(img, 'cm.jpg')
+        plot.save(fig, 'cm.png', format='png')
