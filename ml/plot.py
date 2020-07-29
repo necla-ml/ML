a0b06f0a9fd0968caf5e96085c51961396f7950f
Farley Lai <farleylai@nec-labs.com>
2020-07-29 06:41:19 -0400

    Add close(), set() and params to specify cmap

--- ml/plot.py
+++ ml/plot.py
@@ -1,23 +1,41 @@
 import itertools
-import math
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from ml import io
+matplotlib.use('Agg')
 
-def save(fig, path, format='png'):
-    figure = plt.gcf()
-    plt.figure(fig.number)
+def set(param, value):
+    matplotlib.rcParams[param] = value
+
+def close(fig):
+    plt.close(fig)
+
+def save(fig, path, format='png', close=True):
+    gcf = plt.gcf()
+    bak = fig is not gcf
+    if bak:
+        plt.figure(fig.number)
+    from pathlib import Path
+    path = Path(path)
+    format = format == path.suffix and format or path.suffix[1:]
     plt.savefig(path, format=format)
-    plt.figure(figure.number)
+    if close:
+        plt.close(fig)
+    if bak:
+        plt.figure(gcf.number)
 
 def show(fig):
-    figure = plt.gcf()
-    plt.figure(fig.number)
+    gcf = plt.gcf()
+    bak = fig is not gcf
+    if bak:
+        plt.figure(fig.number)
     plt.show()
-    plt.figure(figure.number)
+    if bak:
+        plt.figure(figure.number)
 
 def numpy(fig, dpi=None):
-    from ml import cv, math
+    from ml import cv
     bio = io.BytesIO()
     fig.savefig(bio, format='raw', dpi=dpi or fig.dpi)
     bio.seek(0)
@@ -27,7 +45,7 @@ def numpy(fig, dpi=None):
     bio.close()
     return img
 
-def confusion_matrix(cm, labels=None, title=None, size=8, dpi=96):
+def confusion_matrix(cm, labels=None, title=None, size=8, dpi=100, interpolation='none', cmap='viridis'):
     """
     Returns a matplotlib figure containing the plotted confusion matrix.
     
@@ -40,29 +58,33 @@ def confusion_matrix(cm, labels=None, title=None, size=8, dpi=96):
     """
     if labels is None:
         labels = [str(c) for c in range(cm.shape[0])]
-    
-    import matplotlib.pyplot as plt
     if isinstance(size, int):
         size = (size, size)
+
+    plt.clf()
     figure = plt.figure(figsize=size, dpi=dpi)
-    plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
-    plt.title(title or "Confusion Matrix")
-    plt.colorbar()
-    tick_marks = np.arange(len(labels))
-    plt.xticks(tick_marks, labels, rotation=45)
-    plt.yticks(tick_marks, labels)
     
-    # Normalize the confusion matrix.
+    # Round the confusion matrix.
     import torch
     if torch.is_tensor(cm):
         cm = cm.numpy()
-    cm = np.around(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis], decimals=2)
+    cm = np.around(cm.astype('float'), decimals=2)
     
-    # Use white text if squares are dark; otherwise black.
-    threshold = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        color = "white" if cm[i, j] > threshold else "black"
-        plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
+#    plt.imshow(cm, interpolation=interpolation, cmap=plt.cm.Blues)
+    plt.imshow(cm, interpolation=interpolation, cmap=plt.get_cmap(cmap))
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation='vertical', fontsize=8)
+    plt.yticks(tick_marks, labels, fontsize=8)
+    plt.title(title or "Confusion Matrix")
+    plt.grid()
+
+    # Draw white text if squares are dark; otherwise black only for small matrix
+    if len(cm) < 51:
+        threshold = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            color = "white" if cm[i, j] > threshold else "black"
+            plt.text(j, i, cm[i, j], horizontalalignment="center", color=color)
         
     plt.ylabel('Ground Truth')
     plt.xlabel('Predictions')
