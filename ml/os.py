42086a0c7385274d2e98a2a6a5d71769169f8791
Farley Lai <farleylai@nec-labs.com>
2020-11-11 12:01:51 -0500

    Import symbols from os

---
+++ ml/os.py
@@ -0,0 +1,11 @@
+from os import *
+from pathlib import Path
+
+def path(filename, mkdir=False):
+    r"""Return Path object and create parent directories if specified.
+    """
+    from pathlib import Path
+    filename = Path(filename)
+    if mkdir:
+        filename.parent.mkdir(parents=True, exist_ok=True)
+    return filename
