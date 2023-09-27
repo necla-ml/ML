bb160b4305a31f25d85e64a9ae2ed0b9567954a2
Erik Kruus <kruus@nec-labs.com>
2023-09-27 16:18:52 -0400

    add autogen file

---
+++ test_ml_imports.py
@@ -0,0 +1,36 @@
+# GENERATED TEST FILE
+# TIME: Wed Sep 27 15:05:51 2023"
+print('import ml ...')
+import ml
+print('import ml.deploy ...')
+import ml.deploy
+print('import ml.sys ...')
+import ml.sys
+print('import ml.analytics ...')
+import ml.analytics
+print('import ml.models ...')
+import ml.models
+print('import ml.distributed ...')
+import ml.distributed
+print('import ml.requests ...')
+import ml.requests
+print('import ml.utils ...')
+import ml.utils
+print('import ml.nn ...')
+import ml.nn
+print('import ml.cuda ...')
+import ml.cuda
+print('import ml.multiprocessing ...')
+import ml.multiprocessing
+print('import ml.data ...')
+import ml.data
+print('import ml.av ...')
+import ml.av
+print('import ml.requests.multipart ...')
+import ml.requests.multipart
+print('import ml.nn.modules ...')
+import ml.nn.modules
+print('import ml.av.transforms ...')
+import ml.av.transforms
+print('import ml.av.backend ...')
+import ml.av.backend
